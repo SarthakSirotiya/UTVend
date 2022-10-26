37ea054faa8c15168175298c04ac1647f7c8fb82
Humberto Torralba <humberto.torralba72@gmail.com>
2022-10-25 19:34:11 -0500

    Connected all activities to intents and halfway done with getting blank google maps thing to work.

--- app/src/main/java/com/trescaballeros/utvend/MainActivity.kt
+++ app/src/main/java/com/trescaballeros/utvend/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.trescaballeros.utvend
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.trescaballeros.utvend.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         // this is our main activity ! -Manuel T!
+
+        binding.mapButton.setOnClickListener {
+            val intent = Intent(this, GoogleMapActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.settingsButton.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.loginButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
