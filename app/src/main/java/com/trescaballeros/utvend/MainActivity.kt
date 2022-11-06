35482f73eaaae06788e25c6d82a14dc2b463f6c9
Sarthak Sirotiya <sarthakssi2@gmail.com>
2022-11-06 00:07:51 -0500

    replace LoginActivity with FirebaseUI authentication, closes #12

--- app/src/main/java/com/trescaballeros/utvend/MainActivity.kt
+++ app/src/main/java/com/trescaballeros/utvend/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.trescaballeros.utvend
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.view.MenuItem
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
 import com.trescaballeros.utvend.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    val signInLauncher = registerForActivityResult(FirebaseAuthUIActivityResultContract()) { res ->
+        onSignInResult(res)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
         // this is our main activity ! -Manuel T!
-
         binding.mapButton.setOnClickListener {
             val intent = Intent(this, MapActivity::class.java)
             startActivity(intent)
@@ -26,10 +35,33 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.loginButton.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
+            startSignInIntent()
         }
+    }
 
+    private fun startSignInIntent() {
+        val providers = listOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            //.setLogo() TODO add logo to login page
+            // should automatically use app default theme
+            // can add theme manually if necessary
+            .build()
+        signInLauncher.launch(signInIntent)
+    }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        if (result.resultCode == Activity.RESULT_OK) {
+            val user = FirebaseAuth.getInstance().currentUser
+            Toast.makeText(this, "Logged in as ${user?.displayName}", Toast.LENGTH_SHORT).show()
+        }
+        else {
+            Log.d("login", "error resultCode of ${result.resultCode}")
+        }
     }
 
 }
