cf9c22a1cee68160ee8926d095799227b0294f05
Humberto Torralba <humberto.torralba72@gmail.com>
2022-11-13 14:29:07 -0600

    Submit activity - geopoint

--- app/src/main/java/com/trescaballeros/utvend/SubmitActivity.kt
+++ app/src/main/java/com/trescaballeros/utvend/SubmitActivity.kt
@@ -1,21 +1,31 @@
 package com.trescaballeros.utvend
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import coil.load
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.GeoPoint
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import com.trescaballeros.utvend.databinding.ActivitySubmitBinding
+import java.io.ByteArrayOutputStream
+import java.util.*
 
-private const val TAG = "MainActivity"
-private const val REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE = 34
+
+//private const val TAG = "MainActivity"
+//private const val REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE = 34
 
 class SubmitActivity : AppCompatActivity(){
     private lateinit var binding: ActivitySubmitBinding
-
     val PICK_IMAGE = 1
-
+    private lateinit var imageData: Uri
 //    private lateinit var locationManager: LocationManager
 //    private lateinit var tvGpsLocation: TextView
 //    private val locationPermissionCode = 2
@@ -51,159 +61,83 @@ class SubmitActivity : AppCompatActivity(){
         }
 
         binding.submitButton.setOnClickListener {
+            val uniqueID = UUID.randomUUID().toString()
+            val badGeo = GeoPoint(0.0,0.0)
+            val geoNotes = binding.geoNotesEditText.getText().toString()
+            val extraNotes = binding.extraNotesEditText.getText().toString()
+            val curTime = Timestamp.now()
+            //val newVM = VendingMachine(uniqueID, badGeo, uniqueID, binding.geoNotesEditText.getText().toString(), binding.extraNotesEditText.getText().toString(), Timestamp.now(), )
+
+            val newVM = hashMapOf(
+                "extra_notes" to extraNotes,
+                "geo_notes" to geoNotes,
+                "image" to uniqueID + ".jpg",
+                "location" to GeoPoint(0.0, 0.0),
+                "timestamp" to Timestamp(Date())
+            )
+
+            FirebaseFirestore.getInstance().collection("vms_1").document(uniqueID).set(newVM)
+
+            val storage = Firebase.storage
+            // Create a storage reference from our app
+            val storageRef = storage.reference
+//
+//            // Create a reference with an initial file path and name
+//            val pathReference = storageRef.child("images/Sunrise_On_Rails.jpg")
+//
+//            // Create a reference to a file from a Google Cloud Storage URI
+//            val gsReference = storage.getReferenceFromUrl("gs://bucket/images/Sunrise_On_Rails.jpg")
+//
+////            // Create a reference from an HTTPS URL
+////            // Note that in the URL, characters are URL escaped!
+////            val httpsReference = storage.getReferenceFromUrl(
+////                "https://firebasestorage.googleapis.com/b/bucket/o/images%20stars.jpg")
+//            val ONE_MEGABYTE: Long = 1024 * 1024
+//            pathReference.getBytes(ONE_MEGABYTE).addOnSuccessListener {
+//                // Data for "images/island.jpg" is returned, use this as needed
+//                binding.submitImageView.load(ONE_MEGABYTE)
+//            }.addOnFailureListener {
+//                // Handle any errors
+//            }
 
 
-            //old if -ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
-            //                && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
 
-//            if(isLocationPermissionGranted()) {
-//                if(isLocationPermissionGranted()) {
-//                    setTitle("fjdlajlkfdsa")
-//                    getLocation()
-//                }else{
-//                    setTitle("else")
-//                }
-//            } else {
-//                setTitle("BIGelse")
-//            }
-//            if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED  && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)  == PackageManager.PERMISSION_GRANTED) {
-//                if(isLocationPermissionGranted()){
-//                    var currentLocation: Location? = null
-//                    lateinit var locationManager: LocationManager
-//                    locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-//
-//                    val hasGps = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
-////------------------------------------------------------//
-//                    val hasNetwork = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
-//
-//                    val gpsLocationListener: LocationListener = object : LocationListener {
-//                        override fun onLocationChanged(location: Location) {
-////                    locationByGps= location
-//                            currentLocation = location
-//                        }
-//
-//                        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
-//                        override fun onProviderEnabled(provider: String) {}
-//                        override fun onProviderDisabled(provider: String) {}
-//                    }
-//                    var locationByGps = -1
-//                    var locationByNetwork = -1
-////------------------------------------------------------//
-//                    val networkLocationListener: LocationListener = object : LocationListener {
-//                        override fun onLocationChanged(location: Location) {
-//                            locationByGps = location
-////                    currentLocation = location
-//                        }
-//
-//                        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
-//                        override fun onProviderEnabled(provider: String) {}
-//                        override fun onProviderDisabled(provider: String) {}
-//                    }
-//
-//                    if (hasGps) {
-//                        locationManager.requestLocationUpdates(
-//                            LocationManager.GPS_PROVIDER,
-//                            5000,
-//                            0F,
-//                            gpsLocationListener
-//                        )
-//                    }
-////------------------------------------------------------//
-//                    if (hasNetwork) {
-//                        locationManager.requestLocationUpdates(
-//                            LocationManager.NETWORK_PROVIDER,
-//                            5000,
-//                            0F,
-//                            networkLocationListener
-//                        )
-//                    }
-//
-//                    val lastKnownLocationByGps =
-//                        locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
-//                    lastKnownLocationByGps?.let {
-//                        locationByGps = lastKnownLocationByGps
-//                    }
-////------------------------------------------------------//
-//                    val lastKnownLocationByNetwork =
-//                        locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
-//                    lastKnownLocationByNetwork?.let {
-//                        locationByNetwork = lastKnownLocationByNetwork
-//                    }
-////------------------------------------------------------//
-//                    val latitude = -1
-//                    val longitude = -1
-//                    if (locationByGps != null && locationByNetwork != null) {
-//                        if (locationByGps.accuracy > locationByNetwork!!.accuracy) {
-//                            currentLocation = locationByGps
-//                            latitude = currentLocation.latitude
-//                            longitude = currentLocation.longitude
-//                            // use latitude and longitude as per your need
-//                        } else {
-//                            currentLocation = locationByNetwork
-//                            latitude = currentLocation.latitude
-//                            longitude = currentLocation.longitude
-//                            // use latitude and longitude as per your need
-//                        }
-//                    }
-//                }
-////
-////
-////
-//            }
-//            }
+            // Create a reference to "mountains.jpg"
+            val mountainsRef = storageRef.child(uniqueID+".jpg")
+
+            // Create a reference to 'images/mountains.jpg'
+            val mountainImagesRef = storageRef.child("images/"+uniqueID+".jpg")
+
+            // Get the data from an ImageView as bytes
+//            binding.submitImageView.isDrawingCacheEnabled = true
+//            binding.submitImageView.buildDrawingCache()
+            val bitmap = (binding.submitImageView.drawable as BitmapDrawable).bitmap
+            val baos = ByteArrayOutputStream()
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+            val data = baos.toByteArray()
+            var uploadTask = mountainsRef.putBytes(data)
+            uploadTask.addOnFailureListener {
+                // Handle unsuccessful uploads
+            }.addOnSuccessListener { taskSnapshot ->
+                // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
+                // ...
+            }
         }
     }
-//    override fun onActivityResult(requestCode: Int, resultCode: Int, ) {
-//        super.onActivityResult(requestCode, resultCode, data)
-//    }
 
 
-    //*&^%$#^#^%$#%^$#^
 
-//    private fun getLocation() {
-//        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-//        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
-//            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), locationPermissionCode)
-//        }
-//        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)) {
-//            var locationByGps = (Location)0
-//            val gpsLocationListener: LocationListener = object : LocationListener {
-//                override fun onLocationChanged(location: Location) {
-//                    locationByGps = location
-//                }
-//
-//                override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
-//                override fun onProviderEnabled(provider: String) {}
-//                override fun onProviderDisabled(provider: String) {}
-//            }
-//            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5f, gpsLocationListener)
-//        }
-//        //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5f)
-//
-////        locationManager.
-//    }
-//    fun onLocationChanged(location: Location) {
-//        //tvGpsLocation = findViewById(R.id.textView)
-//        //tvGpsLocation.text = "Latitude: " + location.latitude + " , Longitude: " + location.longitude
-//    }
-//    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-//        if (requestCode == locationPermissionCode) {
-//            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
-//            }
-//            else {
-//                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
-//            }
-//        }
-//    }
 
-    //*&^%$#^#^%$#%^$#^
+
+
+
+
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if(requestCode == 0) {
             if(resultCode == RESULT_OK) {
+
                 val selectedImage = data?.getData()
                 binding.submitImageView.load(selectedImage)
                 binding.submitButton.isEnabled = true
@@ -224,30 +158,6 @@ class SubmitActivity : AppCompatActivity(){
             }
         }
     }
-
-//    private fun isLocationPermissionGranted(): Boolean {
-//        return if (ActivityCompat.checkSelfPermission(
-//                this,
-//                android.Manifest.permission.ACCESS_COARSE_LOCATION
-//            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-//                this,
-//                android.Manifest.permission.ACCESS_FINE_LOCATION
-//            ) != PackageManager.PERMISSION_GRANTED
-//        ) {
-//            ActivityCompat.requestPermissions(
-//                this,
-//                arrayOf(
-//                    android.Manifest.permission.ACCESS_FINE_LOCATION,
-//                    android.Manifest.permission.ACCESS_COARSE_LOCATION
-//                ),
-//                212
-//                //unsure what this value has to be, it is the "requestCode" and must be >=0
-//            )
-//            false
-//        } else {
-//            true
-//        }
-//    }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         finish()
