b8726ddd52e68ef6c076ed25b0191a74fd5a3f9f
Prime72 <manueltorralba20@gmail.com>
2022-11-13 22:55:48 -0600

    Recycler is working, needs to be updated, closes #3

--- app/src/main/java/com/trescaballeros/utvend/MapActivity.kt
+++ app/src/main/java/com/trescaballeros/utvend/MapActivity.kt
@@ -8,12 +8,24 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import com.trescaballeros.utvend.adapter.VendingMachineAdapter
+import com.trescaballeros.utvend.model.JavaVendingMachine
 
 class MapActivity : AppCompatActivity() {
+
+    // XXX START
+    // DO NOT DELETE -Manuel
+    private lateinit var myAdapter: VendingMachineAdapter
+    // XXX END
 
     private val signInLauncher =
         registerForActivityResult(FirebaseAuthUIActivityResultContract()) { res ->
@@ -25,7 +37,43 @@ class MapActivity : AppCompatActivity() {
         setContentView(R.layout.activity_map)
         setTitle("LIST_LIST") // TODO strings.xml spanish
 
+        // XXX START
+        // DO NOT DELETE -Manuel
+        // Set up query and options with it
+        val query: Query
+        = FirebaseFirestore.getInstance().collection("vms_1")
+        val options: FirestoreRecyclerOptions<JavaVendingMachine>
+        = FirestoreRecyclerOptions.Builder<JavaVendingMachine>().setQuery(query, JavaVendingMachine::class.java).build()
+
+        // Create VendingMachineAdapter (FirestoreRecyclerAdapter)
+
+        myAdapter = VendingMachineAdapter(this, options)
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view_id)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.setHasFixedSize(true) // maybe remove this
+        recyclerView.adapter = myAdapter
+        setTitle("list num: " + myAdapter.itemCount)
+//
+//        setTitle("getItemCt: " + myAdapter.getItemCount())
+        // XXX END
+
+
+
     }
+
+    // XXX START
+    // DO NOT DELETE -Manuel
+    override fun onStart() {
+        super.onStart()
+        myAdapter.startListening()
+    }
+
+    // XXX
+    override fun onStop() {
+        super.onStop()
+        myAdapter.stopListening()
+    }
+    // XXX END
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.list_menu, menu)
