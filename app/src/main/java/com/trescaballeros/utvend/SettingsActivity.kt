c5a27bc835b881d1b523e57b828be659acb04af1
Sarthak Sirotiya <sarthakssi2@gmail.com>
2022-11-13 23:50:10 -0600

    create toasts for buttons that do nothing yet

--- app/src/main/java/com/trescaballeros/utvend/SettingsActivity.kt
+++ app/src/main/java/com/trescaballeros/utvend/SettingsActivity.kt
@@ -3,15 +3,28 @@ package com.trescaballeros.utvend
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
+import com.trescaballeros.utvend.databinding.ActivitySettingsBinding
 
 class SettingsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySettingsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.lightDarkButton.setOnClickListener {
+            Toast.makeText(this, "yeah this doesn't do anything yet", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.englishSpanishButton.setOnClickListener {
+            Toast.makeText(this, "this doesn't do anything yet either", Toast.LENGTH_SHORT).show()
+        }
 
         // Lab3
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         setTitle("SETTINGS") // TODO strings.xml spanish
+
 
     }
 
